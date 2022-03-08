e815014b28e5057480b113fcaea29b914aaf5151
Franciel Leandro <francielleandro@gmail.com>
2022-03-08 05:16:17 -0300

    trabalhando com css e SASS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import styles from '../styles/styles.module.scss';
+
 export default function Home() {
   return (
-   <div><h1>Board</h1></div>
+   <div><h1 className={styles.title}>Exemplo de SCSS com cascata:<span> SPAN </span></h1></div>
   )
 }
