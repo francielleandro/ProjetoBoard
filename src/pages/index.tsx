f6317c1c4f2b48025812d1100fcbb1e76c542030
Franciel Leandro <francielleandro@gmail.com>
2022-03-08 05:32:58 -0300

    adicionando a propriedade head nos componentes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import styles from '../styles/styles.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-   <div><h1 className={styles.title}>Exemplo de SCSS com cascata:<span> SPAN </span></h1></div>
+    <>
+    <Head>
+      <title>Board</title>
+    </Head>
+    <div>
+      <h1 className={styles.title}>Exemplo de SCSS com cascata:<span> SPAN </span></h1>
+    </div>
+    </>
   )
 }
